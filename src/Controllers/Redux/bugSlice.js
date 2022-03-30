74844b30863c610a7590f24464f671c9c8b22882
Scott Cho <61069878+scotttimothycho@users.noreply.github.com>
2022-03-30 07:05:55 -0700

    bug and auth slices

---
+++ src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name: "bug",
+    initialState: [],
+    reducers: {
+        getBugs: ( state ) => {
+    
+        },
+        createBugs: (state, actions) => {
+
+        },
+        updateBug: (state, actions) => {
+            
+        },
+        markComplete: (state, action) => {
+            
+        }
+    }
+})
